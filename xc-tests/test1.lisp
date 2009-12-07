c0aa09695d0cf90b2ff836d2c12039799fb8eb5b
Alastair Bridgewater <nyef_sbcl@lisphacker.com>
2009-12-07 10:39:09 -0500

    xc-test improvements.
      * Added a test for keyword argument parsing, &rest list accumulation,
    and APPLY of an &rest list.

--- xc-tests/test1.lisp
+++ xc-tests/test1.lisp
@@ -10,6 +10,24 @@
 (defun !cold-init ()
   (%primitive print "initial function start")
   (no-arg-test)
+  (rest-key-args-test :foo :bar :baz :quux)
   (%primitive sb!c:halt))
+
+(defun rest-key-args-test (&rest initargs
+                           &key (args nil argsp)
+                           &allow-other-keys)
+  (if argsp
+      args
+      (apply #'rest-key-args-test-2 initargs)))
+
+(defun rest-key-args-test-2 (a1 a2 a3 a4)
+  (when (eq a1 :foo)
+    (%primitive print "rkat2: foo is right"))
+  (when (eq a2 :bar)
+    (%primitive print "rkat2: bar is right"))
+  (when (eq a3 :baz)
+    (%primitive print "rkat2: baz is right"))
+  (when (eq a4 :quux)
+    (%primitive print "rkat2: quux is right")))
 
 ;;; EOF
