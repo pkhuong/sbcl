b7876abaa68568d92a3b3916fa6bfcbfa1b27353
Paul Khuong <pvk@pvk.ca>
2012-05-30 11:00:02 -0400

    Clean up the cold init stuff

--- src/code/early-cached-specialization.lisp
+++ src/code/early-cached-specialization.lisp
@@ -3,13 +3,14 @@
 (defvar *specialized-function-cache* (make-hash-table
                                       :test 'equal))
 (!cold-init-forms
-  (setq *specialized-function-cache* (make-hash-table :test 'equal
-                                                      :weakness :value)))
+  (setq *specialized-function-cache* (make-hash-table
+                                      :test 'equal
+                                      #-sb-xc-host :weakness #-sb-xc-host :value)))
 
 (defun ensure-specialized-function (key function)
-  (unless (boundp 'sb!impl::*specialized-function-cache*)
+  (unless (boundp '*specialized-function-cache*)
     (return-from ensure-specialized-function function))
-  (let ((table sb!impl::*specialized-function-cache*))
+  (let ((table *specialized-function-cache*))
     (#-sb-xc-host with-locked-hash-table #-sb-xc-host (table)
      #+sb-xc-host progn
       (or (gethash key table)
